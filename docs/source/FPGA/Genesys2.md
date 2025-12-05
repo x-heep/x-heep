9435518a70ebf8a2b8d0e3f54d0b771ecb8fb379
danivz <daniel.vazquez@upm.es>
2025-12-05 19:31:14 +0100

    Port to genesys2 dev board

---
+++ docs/source/FPGA/Genesys2.md
@@ -0,0 +1,5 @@
+# FPGA Genesys2: Connection Guide
+
+This section shows a diagram illustrating how to connect the EPFL X-HEEP programmer to the Genesys2 development board. Although the board provides two PMOD pairs, the programmer must be connected specifically to the interface shown in the image for attaching the external FTDI and flash memory.
+
+<img src="./../images/genesys-2-2.png" width="500">
--- hw/fpga/xilinx_core_v_mini_mcu_wrapper.sv
+++ hw/fpga/xilinx_core_v_mini_mcu_wrapper.sv
@@ -14,18 +14,21 @@ module xilinx_core_v_mini_mcu_wrapper
 ) (
 
 `ifdef FPGA_ZCU104
-    inout logic clk_300mhz_n,
-    inout logic clk_300mhz_p,
+    inout  logic clk_300mhz_n,
+    inout  logic clk_300mhz_p,
 `elsif FPGA_ZCU102
     inout logic clk_125mhz_n,
     inout logic clk_125mhz_p,
 `elsif FPGA_AUP_ZU3
     inout logic clk_100mhz_n,
     inout logic clk_100mhz_p,
+`elsif FPGA_GENESYS2
+    inout  logic clk_200mhz_n,
+    inout  logic clk_200mhz_p,
 `else
-    inout logic clk_i,
+    inout  logic clk_i,
 `endif
-    inout logic rst_i,
+    inout  logic rst_i,
 
     output logic rst_led_o,
     output logic clk_led_o,
@@ -84,6 +87,8 @@ module xilinx_core_v_mini_mcu_wrapper
   // low active reset
 `ifdef FPGA_NEXYS
   assign rst_n = rst_i;
+`elsif FPGA_GENESYS2
+  assign rst_n = rst_i;
 `else
   assign rst_n = !rst_i;
 `endif
@@ -121,6 +126,12 @@ module xilinx_core_v_mini_mcu_wrapper
   xilinx_clk_wizard_wrapper xilinx_clk_wizard_wrapper_i (
       .CLK_IN1_D_0_clk_n(clk_100mhz_n),
       .CLK_IN1_D_0_clk_p(clk_100mhz_p),
+      .clk_out1_0(clk_gen)
+  );
+`elsif FPGA_GENESYS2
+  xilinx_clk_wizard_wrapper xilinx_clk_wizard_wrapper_i (
+      .CLK_IN1_D_0_clk_n(clk_200mhz_n),
+      .CLK_IN1_D_0_clk_p(clk_200mhz_p),
       .clk_out1_0(clk_gen)
   );
 `elsif FPGA_NEXYS

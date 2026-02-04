62e748d239b1e0f7536f374f04f00af7ef514ac8
Burdina Anna <burdina@eslpc11.intranet.epfl.ch>
2026-02-04 11:09:16 +0100

    added comments todo for sl application

--- sw/applications/example_serial_link_simulation_dma/main.c
+++ sw/applications/example_serial_link_simulation_dma/main.c
@@ -14,6 +14,7 @@
 #define DMA_DATA_LARGE 8 
 #define TEST_DATA_LARGE 16
 
+// TODO: test on 8 and 16bits and unaligned addresses
 static uint32_t to_be_sent_4B[TEST_DATA_LARGE] __attribute__((aligned(4))) = {0};
 static uint32_t copied_data_4B[TEST_DATA_LARGE] __attribute__((aligned(4))) = {0};
 

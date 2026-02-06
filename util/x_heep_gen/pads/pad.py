d932718a955cc8fd6841df6a810316371ce6d9f0
Juan Sapriza <juan.sapriza@epfl.ch>
2026-02-06 15:36:27 +0000

    format python sorry i know i know ....

--- util/x_heep_gen/pads/pad.py
+++ util/x_heep_gen/pads/pad.py
@@ -54,7 +54,7 @@ class Pad:
         # to skip the corner iocell, which does not have a bondpad.
         # Only used during floorplanning.
         self.offset: float = None
-        
+
         # The space in um from the edge of the ring that this pad belongs to, to the center of this
         # pad. This is computed automatically during the build process in the pad ring, but can be
         # hardcoded in advance in case you want a pad in a specific location in the padring. In the

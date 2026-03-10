7a58d8188b88ccb9a659a105011f2344518a7883
Schiavone Pasquale Davide <schiavon@eslsrv12.intranet.epfl.ch>
2026-03-10 20:27:44 +0100

    tested app

--- sw/applications/example_power_manager/main.c
+++ sw/applications/example_power_manager/main.c
@@ -91,244 +91,244 @@ int main(int argc, char *argv[])
     // Setup plic
     plic_Init();
 
-//     // Get current Frequency
-//     soc_ctrl_t soc_ctrl;
-//     soc_ctrl.base_addr = mmio_region_from_addr((uintptr_t)SOC_CTRL_START_ADDRESS);
-//     uint32_t freq_hz = soc_ctrl_get_frequency(&soc_ctrl);
+    // Get current Frequency
+    soc_ctrl_t soc_ctrl;
+    soc_ctrl.base_addr = mmio_region_from_addr((uintptr_t)SOC_CTRL_START_ADDRESS);
+    uint32_t freq_hz = soc_ctrl_get_frequency(&soc_ctrl);
 
-//     // Setup rv_timer_0_1
-//     mmio_region_t timer_0_1_reg = mmio_region_from_addr(RV_TIMER_AO_START_ADDRESS);
-//     rv_timer_init(timer_0_1_reg, (rv_timer_config_t){.hart_count = 2, .comparator_count = 1}, &timer_0_1);
-//     rv_timer_tick_params_t tick_params;
-//     rv_timer_approximate_tick_params(freq_hz, kTickFreqHz, &tick_params);
+    // Setup rv_timer_0_1
+    mmio_region_t timer_0_1_reg = mmio_region_from_addr(RV_TIMER_AO_START_ADDRESS);
+    rv_timer_init(timer_0_1_reg, (rv_timer_config_t){.hart_count = 2, .comparator_count = 1}, &timer_0_1);
+    rv_timer_tick_params_t tick_params;
+    rv_timer_approximate_tick_params(freq_hz, kTickFreqHz, &tick_params);
 
-//     // Setup rv_timer_2_3
-//     mmio_region_t timer_2_3_reg = mmio_region_from_addr(RV_TIMER_START_ADDRESS);
-//     rv_timer_init(timer_2_3_reg, (rv_timer_config_t){.hart_count = 2, .comparator_count = 1}, &timer_2_3);
+    // Setup rv_timer_2_3
+    mmio_region_t timer_2_3_reg = mmio_region_from_addr(RV_TIMER_START_ADDRESS);
+    rv_timer_init(timer_2_3_reg, (rv_timer_config_t){.hart_count = 2, .comparator_count = 1}, &timer_2_3);
 
-//     // Init cpu_subsystem's counters
+    // Init cpu_subsystem's counters
 
-//     //Turn off: first, isolate the CPU outputs, then I reset it, then I switch it off (reset and switch off order does not really matter)
-//     iso_off = 10;
-//     reset_off = iso_off + 5;
-//     switch_off = reset_off + 5;
-//     //Turn on: first, give back power by switching on, then deassert the reset, the unisolate the CPU outputs
-//     switch_on = 10;
-//     reset_on = switch_on + 20; //give 20 cycles to emulate the turn on time, this number depends on technology and here it is just a random number
-//     iso_on = reset_on + 5;
+    //Turn off: first, isolate the CPU outputs, then I reset it, then I switch it off (reset and switch off order does not really matter)
+    iso_off = 10;
+    reset_off = iso_off + 5;
+    switch_off = reset_off + 5;
+    //Turn on: first, give back power by switching on, then deassert the reset, the unisolate the CPU outputs
+    switch_on = 10;
+    reset_on = switch_on + 20; //give 20 cycles to emulate the turn on time, this number depends on technology and here it is just a random number
+    iso_on = reset_on + 5;
 
-//     if (power_manager_pwr_gate_counters_init(&power_manager_counters, reset_off, reset_on, switch_off, switch_on, iso_off, iso_on, 0, 0) != kPowerManagerOk_e)
-//     {
-//         PRINTF("Error: power manager fail. Check the reset and powergate counters value\n\r");
-//         return EXIT_FAILURE;
-//     }
+    if (power_manager_pwr_gate_counters_init(&power_manager_counters, reset_off, reset_on, switch_off, switch_on, iso_off, iso_on, 0, 0) != kPowerManagerOk_e)
+    {
+        PRINTF("Error: power manager fail. Check the reset and powergate counters value\n\r");
+        return EXIT_FAILURE;
+    }
 
-//     // Power-gate and wake-up due to timer_0
-//     rv_timer_set_tick_params(&timer_0_1, 0, tick_params);
-//     rv_timer_irq_enable(&timer_0_1, 0, 0, kRvTimerEnabled);
-//     rv_timer_arm(&timer_0_1, 0, 0, 64);
-//     rv_timer_counter_set_enabled(&timer_0_1, 0, kRvTimerEnabled);
+    // Power-gate and wake-up due to timer_0
+    rv_timer_set_tick_params(&timer_0_1, 0, tick_params);
+    rv_timer_irq_enable(&timer_0_1, 0, 0, kRvTimerEnabled);
+    rv_timer_arm(&timer_0_1, 0, 0, 64);
+    rv_timer_counter_set_enabled(&timer_0_1, 0, kRvTimerEnabled);
 
-//     CSR_CLEAR_BITS(CSR_REG_MSTATUS, 0x8);
-//     if (power_manager_pwr_gate_core(kTimer_0_pm_e, &power_manager_counters) != kPowerManagerOk_e)
-//     {
-//         PRINTF("Error: power manager fail.\n\r");
-//         return EXIT_FAILURE;
-//     }
-//     CSR_SET_BITS(CSR_REG_MSTATUS, 0x8);
+    CSR_CLEAR_BITS(CSR_REG_MSTATUS, 0x8);
+    if (power_manager_pwr_gate_core(kTimer_0_pm_e, &power_manager_counters) != kPowerManagerOk_e)
+    {
+        PRINTF("Error: power manager fail.\n\r");
+        return EXIT_FAILURE;
+    }
+    CSR_SET_BITS(CSR_REG_MSTATUS, 0x8);
 
-//     // Power-gate and wake-up due to timer_1
-//     rv_timer_set_tick_params(&timer_0_1, 1, tick_params);
-//     rv_timer_irq_enable(&timer_0_1, 1, 0, kRvTimerEnabled);
-//     rv_timer_arm(&timer_0_1, 1, 0, 64);
-//     rv_timer_counter_set_enabled(&timer_0_1, 1, kRvTimerEnabled);
+    // Power-gate and wake-up due to timer_1
+    rv_timer_set_tick_params(&timer_0_1, 1, tick_params);
+    rv_timer_irq_enable(&timer_0_1, 1, 0, kRvTimerEnabled);
+    rv_timer_arm(&timer_0_1, 1, 0, 64);
+    rv_timer_counter_set_enabled(&timer_0_1, 1, kRvTimerEnabled);
 
-//     CSR_CLEAR_BITS(CSR_REG_MSTATUS, 0x8);
-//     if (power_manager_pwr_gate_core(kTimer_1_pm_e, &power_manager_counters) != kPowerManagerOk_e)
-//     {
-//         PRINTF("Error: power manager fail.\n\r");
-//         return EXIT_FAILURE;
-//     }
-//     CSR_SET_BITS(CSR_REG_MSTATUS, 0x8);
+    CSR_CLEAR_BITS(CSR_REG_MSTATUS, 0x8);
+    if (power_manager_pwr_gate_core(kTimer_1_pm_e, &power_manager_counters) != kPowerManagerOk_e)
+    {
+        PRINTF("Error: power manager fail.\n\r");
+        return EXIT_FAILURE;
+    }
+    CSR_SET_BITS(CSR_REG_MSTATUS, 0x8);
 
-//     // Power-gate and wake-up due to timer_2
-//     rv_timer_set_tick_params(&timer_2_3, 0, tick_params);
-//     rv_timer_irq_enable(&timer_2_3, 0, 0, kRvTimerEnabled);
-//     rv_timer_arm(&timer_2_3, 0, 0, 70);
-//     rv_timer_counter_set_enabled(&timer_2_3, 0, kRvTimerEnabled);
+    // Power-gate and wake-up due to timer_2
+    rv_timer_set_tick_params(&timer_2_3, 0, tick_params);
+    rv_timer_irq_enable(&timer_2_3, 0, 0, kRvTimerEnabled);
+    rv_timer_arm(&timer_2_3, 0, 0, 70);
+    rv_timer_counter_set_enabled(&timer_2_3, 0, kRvTimerEnabled);
 
-//     CSR_CLEAR_BITS(CSR_REG_MSTATUS, 0x8);
-//     if (power_manager_pwr_gate_core(kTimer_2_pm_e, &power_manager_counters) != kPowerManagerOk_e)
-//     {
-//         PRINTF("Error: power manager fail.\n\r");
-//         return EXIT_FAILURE;
-//     }
-//     CSR_SET_BITS(CSR_REG_MSTATUS, 0x8);
+    CSR_CLEAR_BITS(CSR_REG_MSTATUS, 0x8);
+    if (power_manager_pwr_gate_core(kTimer_2_pm_e, &power_manager_counters) != kPowerManagerOk_e)
+    {
+        PRINTF("Error: power manager fail.\n\r");
+        return EXIT_FAILURE;
+    }
+    CSR_SET_BITS(CSR_REG_MSTATUS, 0x8);
 
-//     // Power-gate and wake-up due to timer_3
-//     rv_timer_set_tick_params(&timer_2_3, 1, tick_params);
-//     rv_timer_irq_enable(&timer_2_3, 1, 0, kRvTimerEnabled);
-//     rv_timer_arm(&timer_2_3, 1, 0, 70);
-//     rv_timer_counter_set_enabled(&timer_2_3, 1, kRvTimerEnabled);
+    // Power-gate and wake-up due to timer_3
+    rv_timer_set_tick_params(&timer_2_3, 1, tick_params);
+    rv_timer_irq_enable(&timer_2_3, 1, 0, kRvTimerEnabled);
+    rv_timer_arm(&timer_2_3, 1, 0, 70);
+    rv_timer_counter_set_enabled(&timer_2_3, 1, kRvTimerEnabled);
 
-//     CSR_CLEAR_BITS(CSR_REG_MSTATUS, 0x8);
-//     if (power_manager_pwr_gate_core(kTimer_3_pm_e, &power_manager_counters) != kPowerManagerOk_e)
-//     {
-//         PRINTF("Error: power manager fail.\n\r");
-//         return EXIT_FAILURE;
-//     }
-//     CSR_SET_BITS(CSR_REG_MSTATUS, 0x8);
+    CSR_CLEAR_BITS(CSR_REG_MSTATUS, 0x8);
+    if (power_manager_pwr_gate_core(kTimer_3_pm_e, &power_manager_counters) != kPowerManagerOk_e)
+    {
+        PRINTF("Error: power manager fail.\n\r");
+        return EXIT_FAILURE;
+    }
+    CSR_SET_BITS(CSR_REG_MSTATUS, 0x8);
 
-//     // Power-gate and wake-up due to DMA
+    // Power-gate and wake-up due to DMA
 
-//     //prepare data
-//     for(int i=0; i<TEST_DATA_SIZE;i++){
-//         test_data_4B[i] = i;
-//     }
+    //prepare data
+    for(int i=0; i<TEST_DATA_SIZE;i++){
+        test_data_4B[i] = i;
+    }
 
-//     dma_init(NULL);
-//     static dma_config_flags_t res;
-//     static dma_target_t tgt_src;
-//     static dma_target_t tgt_dst;
-//     static dma_trans_t trans;
+    dma_init(NULL);
+    static dma_config_flags_t res;
+    static dma_target_t tgt_src;
+    static dma_target_t tgt_dst;
+    static dma_trans_t trans;
 
-//     // Initialize the DMA for the next tests
-//     tgt_src.ptr = (uint8_t *)test_data_4B;
-//     tgt_src.inc_d1_du = 1;
-//     tgt_src.trig = DMA_TRIG_MEMORY;
-//     tgt_src.type = DMA_DATA_TYPE_WORD;
-//     tgt_src.env = NULL;
-//     tgt_src.inc_d2_du = 0;
+    // Initialize the DMA for the next tests
+    tgt_src.ptr = (uint8_t *)test_data_4B;
+    tgt_src.inc_d1_du = 1;
+    tgt_src.trig = DMA_TRIG_MEMORY;
+    tgt_src.type = DMA_DATA_TYPE_WORD;
+    tgt_src.env = NULL;
+    tgt_src.inc_d2_du = 0;
 
-//     tgt_dst.ptr = (uint8_t *)copied_data_4B;
-//     tgt_dst.inc_d1_du = 1;
-//     tgt_dst.trig = DMA_TRIG_MEMORY;
-//     tgt_dst.type = DMA_DATA_TYPE_WORD;
-//     tgt_dst.env = NULL;
-//     tgt_dst.inc_d2_du = 0;
+    tgt_dst.ptr = (uint8_t *)copied_data_4B;
+    tgt_dst.inc_d1_du = 1;
+    tgt_dst.trig = DMA_TRIG_MEMORY;
+    tgt_dst.type = DMA_DATA_TYPE_WORD;
+    tgt_dst.env = NULL;
+    tgt_dst.inc_d2_du = 0;
 
-//     trans.size_d1_du = TEST_DATA_SIZE;
-//     trans.src = &tgt_src;
-//     trans.dst = &tgt_dst;
-//     trans.src_type = DMA_DATA_TYPE_WORD;
-//     trans.dst_type = DMA_DATA_TYPE_WORD;
-//     trans.mode = DMA_TRANS_MODE_SINGLE;
-//     trans.win_du = 0;
-//     trans.sign_ext = 0;
-//     trans.end = DMA_TRANS_END_INTR;
-//     trans.dim = DMA_DIM_CONF_1D;
-//     trans.dim_inv = 0;
-//     trans.channel = 0;
+    trans.size_d1_du = TEST_DATA_SIZE;
+    trans.src = &tgt_src;
+    trans.dst = &tgt_dst;
+    trans.src_type = DMA_DATA_TYPE_WORD;
+    trans.dst_type = DMA_DATA_TYPE_WORD;
+    trans.mode = DMA_TRANS_MODE_SINGLE;
+    trans.win_du = 0;
+    trans.sign_ext = 0;
+    trans.end = DMA_TRANS_END_INTR;
+    trans.dim = DMA_DIM_CONF_1D;
+    trans.dim_inv = 0;
+    trans.channel = 0;
 
-//     trans.pad_top_du = 0;
-//     trans.pad_bottom_du = 0;
-//     trans.pad_left_du = 0;
-//     trans.pad_right_du = 0;
+    trans.pad_top_du = 0;
+    trans.pad_bottom_du = 0;
+    trans.pad_left_du = 0;
+    trans.pad_right_du = 0;
 
-//     trans.flags = 0x0;
-//     res = dma_validate_transaction(&trans, DMA_ENABLE_REALIGN, DMA_PERFORM_CHECKS_INTEGRITY);
-//     res = dma_load_transaction(&trans);
-//     res = dma_launch(&trans);
+    trans.flags = 0x0;
+    res = dma_validate_transaction(&trans, DMA_ENABLE_REALIGN, DMA_PERFORM_CHECKS_INTEGRITY);
+    res = dma_load_transaction(&trans);
+    res = dma_launch(&trans);
 
-//     while (!dma_is_ready(0))
-//     {
-//         CSR_CLEAR_BITS(CSR_REG_MSTATUS, 0x8);
-//         if (dma_is_ready(0) == 0)
-//         {
-//                 if (power_manager_pwr_gate_core(kDma_pm_e, &power_manager_counters) != kPowerManagerOk_e)
-//                 {
-//                     PRINTF("Error: power manager fail.\n\r");
-//                     return EXIT_FAILURE;
-//                 }
-//         }
-//         CSR_SET_BITS(CSR_REG_MSTATUS, 0x8);
-//     }
+    while (!dma_is_ready(0))
+    {
+        CSR_CLEAR_BITS(CSR_REG_MSTATUS, 0x8);
+        if (dma_is_ready(0) == 0)
+        {
+                if (power_manager_pwr_gate_core(kDma_pm_e, &power_manager_counters) != kPowerManagerOk_e)
+                {
+                    PRINTF("Error: power manager fail.\n\r");
+                    return EXIT_FAILURE;
+                }
+        }
+        CSR_SET_BITS(CSR_REG_MSTATUS, 0x8);
+    }
 
-//     for(int i=0; i<TEST_DATA_SIZE; i++) {
-//         if (copied_data_4B[i] != test_data_4B[i]) {
-//             PRINTF("ERROR COPY [%d]: %08x != %08x : %04x != %04x\n", i, &copied_data_4B[i], &test_data_4B[i], copied_data_4B[i], test_data_4B[i]);
-//             return -1;
-//         }
-//     }
+    for(int i=0; i<TEST_DATA_SIZE; i++) {
+        if (copied_data_4B[i] != test_data_4B[i]) {
+            PRINTF("ERROR COPY [%d]: %08x != %08x : %04x != %04x\n", i, &copied_data_4B[i], &test_data_4B[i], copied_data_4B[i], test_data_4B[i]);
+            return -1;
+        }
+    }
 
-// #ifndef TARGET_IS_FPGA
-//     // Power-gate and wake-up due to plic GPIO
+#ifndef TARGET_IS_FPGA
+    // Power-gate and wake-up due to plic GPIO
 
-//     gpio_assign_irq_handler( GPIO_INTR_31, &gpio_handler_in );
+    gpio_assign_irq_handler( GPIO_INTR_31, &gpio_handler_in );
 
-//     bool state = false;
-//     plic_irq_set_priority(GPIO_INTR_31, 1);
-//     plic_irq_set_enabled(GPIO_INTR_31, kPlicToggleEnabled);
+    bool state = false;
+    plic_irq_set_priority(GPIO_INTR_31, 1);
+    plic_irq_set_enabled(GPIO_INTR_31, kPlicToggleEnabled);
 
-//     gpio_cfg_t pin2_cfg = {.pin = GPIO_TB_IN, .mode = GpioModeIn,.en_input_sampling = true,
-//         .en_intr = true, .intr_type = GpioIntrEdgeRising};
-//     gpio_config (pin2_cfg);
+    gpio_cfg_t pin2_cfg = {.pin = GPIO_TB_IN, .mode = GpioModeIn,.en_input_sampling = true,
+        .en_intr = true, .intr_type = GpioIntrEdgeRising};
+    gpio_config (pin2_cfg);
 
-//     gpio_cfg_t pin1_cfg = {.pin = GPIO_TB_OUT, .mode = GpioModeOutPushPull};
-//     gpio_config (pin1_cfg);
-//     gpio_write(GPIO_TB_OUT, true);
+    gpio_cfg_t pin1_cfg = {.pin = GPIO_TB_OUT, .mode = GpioModeOutPushPull};
+    gpio_config (pin1_cfg);
+    gpio_write(GPIO_TB_OUT, true);
 
-//     CSR_CLEAR_BITS(CSR_REG_MSTATUS, 0x8);
-//     if (power_manager_pwr_gate_core(kPlic_pm_e, &power_manager_counters) != kPowerManagerOk_e)
-//     {
-//         PRINTF("Error: power manager fail.\n\r");
-//         return EXIT_FAILURE;
-//     }
-//     CSR_SET_BITS(CSR_REG_MSTATUS, 0x8);
+    CSR_CLEAR_BITS(CSR_REG_MSTATUS, 0x8);
+    if (power_manager_pwr_gate_core(kPlic_pm_e, &power_manager_counters) != kPowerManagerOk_e)
+    {
+        PRINTF("Error: power manager fail.\n\r");
+        return EXIT_FAILURE;
+    }
+    CSR_SET_BITS(CSR_REG_MSTATUS, 0x8);
 
-//     uint32_t intr_num;
-//     plic_irq_complete(&intr_num);
+    uint32_t intr_num;
+    plic_irq_complete(&intr_num);
 
-//     if(gpio_intr_flag == 0){
-//         PRINTF("Error: GPIO interrupt not detected\n\r");
-//         return EXIT_FAILURE;
-//     }
+    if(gpio_intr_flag == 0){
+        PRINTF("Error: GPIO interrupt not detected\n\r");
+        return EXIT_FAILURE;
+    }
 
-// #endif
+#endif
 
-//     /* write something to stdout */
-//     PRINTF("CPU Power Gating Test Successefull\n\r");
+    /* write something to stdout */
+    PRINTF("CPU Power Gating Test Successefull\n\r");
 
-//     // ------------ PERIPHERAL SUBSYSTEM  ------------
-//     PRINTF("Testing Peripheral Subsystem...\n\r");
+    // ------------ PERIPHERAL SUBSYSTEM  ------------
+    PRINTF("Testing Peripheral Subsystem...\n\r");
 
-//     // ------------ clock gating ------------
-//     power_manager_clk_gate_periph(0x1);
-//     // Wait some time
-//     for (int i=0; i<100; i++) asm volatile("nop;");
-//     // Enabling the peripheral subsystem
-//     power_manager_clk_gate_periph(0x0);
+    // ------------ clock gating ------------
+    power_manager_clk_gate_periph(0x1);
+    // Wait some time
+    for (int i=0; i<100; i++) asm volatile("nop;");
+    // Enabling the peripheral subsystem
+    power_manager_clk_gate_periph(0x0);
 
-//     PRINTF("Peripheral Clock Gating Test Successefull\n\r");
+    PRINTF("Peripheral Clock Gating Test Successefull\n\r");
 
-//     // ------------ power gating ------------
-//     if (power_manager_pwr_gate_counters_init(&power_manager_counters, 30, 30, 30, 30, 30, 30, 0, 0) != kPowerManagerOk_e)
-//     {
-//         PRINTF("Error: power manager fail. Check the reset and powergate counters value\n\r");
-//         return EXIT_FAILURE;
-//     }
+    // ------------ power gating ------------
+    if (power_manager_pwr_gate_counters_init(&power_manager_counters, 30, 30, 30, 30, 30, 30, 0, 0) != kPowerManagerOk_e)
+    {
+        PRINTF("Error: power manager fail. Check the reset and powergate counters value\n\r");
+        return EXIT_FAILURE;
+    }
 
-//     // Power off peripheral_subsystem domain
-//     if (power_manager_pwr_gate_periph(kOff_e, &power_manager_counters) != kPowerManagerOk_e)
-//     {
-//         PRINTF("Error: power manager fail.\n\r");
-//         return EXIT_FAILURE;
-//     }
+    // Power off peripheral_subsystem domain
+    if (power_manager_pwr_gate_periph(kOff_e, &power_manager_counters) != kPowerManagerOk_e)
+    {
+        PRINTF("Error: power manager fail.\n\r");
+        return EXIT_FAILURE;
+    }
 
-//     // Check that the peripheral_subsystem domain is actually OFF
-//     while(!power_manager_periph_domain_is_off());
+    // Check that the peripheral_subsystem domain is actually OFF
+    while(!power_manager_periph_domain_is_off());
 
-//     // Wait some time
-//     for (int i=0; i<100; i++) asm volatile("nop;");
+    // Wait some time
+    for (int i=0; i<100; i++) asm volatile("nop;");
 
-//     // Power on peripheral_subsystem domain
-//     if (power_manager_pwr_gate_periph(kOn_e, &power_manager_counters) != kPowerManagerOk_e)
-//     {
-//         PRINTF("Error: power manager fail.\n\r");
-//         return EXIT_FAILURE;
-//     }
+    // Power on peripheral_subsystem domain
+    if (power_manager_pwr_gate_periph(kOn_e, &power_manager_counters) != kPowerManagerOk_e)
+    {
+        PRINTF("Error: power manager fail.\n\r");
+        return EXIT_FAILURE;
+    }
 
-//     PRINTF("Peripheral Power Gating Test Successefull\n\r");
+    PRINTF("Peripheral Power Gating Test Successefull\n\r");
 
 #if MEMORY_BANKS > 2
     // ------------ MEMORY SUBSYSTEM  ------------

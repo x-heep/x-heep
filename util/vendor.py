c06b87e8e9a775a280a692fcec0af2797e19f849
Luigi Giuffrida <luigi.giuffrida.98@gmail.com>
2026-04-22 15:35:00 +0200

    Avoid generation of a single lock file

--- util/vendor.py
+++ util/vendor.py
@@ -551,9 +551,8 @@ class Desc:
                 if "target_dir" not in project_data:
                     project_data["target_dir"] = str(Path(vendor_name) / project_name)
                 desc = Desc(path, project_data, desc_overrides)
-                # Multi-entry files share a single lock file.
-                desc.use_named_lock_entry = True
-                desc._lock_file_override = path.with_name(vendor_name + ".lock.hjson")
+                desc.use_named_lock_entry = False
+                desc._lock_file_override = path.with_name(project_name + ".lock.hjson")
                 descs.append(desc)
 
             if module_filter and not descs:

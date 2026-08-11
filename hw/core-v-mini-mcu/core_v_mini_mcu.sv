0111a74e9b84d6b921d555941246d5134958a298
David Mallasén <david.mallasen@epfl.ch>
2026-08-11 17:50:59 +0200

    Update generated core_v_mini_mcu

--- hw/core-v-mini-mcu/core_v_mini_mcu.sv
+++ hw/core-v-mini-mcu/core_v_mini_mcu.sv
@@ -547,6 +547,7 @@ module core_v_mini_mcu #(
       .debug_master_resp_i(debug_master_resp)
   );
 
+
   system_bus #(
       .NUM_BANKS(core_v_mini_mcu_pkg::NUM_BANKS),
       .EXT_XBAR_NMASTER(EXT_XBAR_NMASTER),
@@ -705,7 +706,7 @@ module core_v_mini_mcu #(
       .cio_sda_i(i2c_sda_i),
       .cio_sda_o(i2c_sda_o),
       .cio_sda_en_o(i2c_sda_oe_o),
-      .spi_sck_o,
+      .spi_sck_o(spi_sck_o),
       .spi_sck_en_o(spi_sck_oe_o),
       .spi_csb_o({spi_cs_1_o, spi_cs_0_o}),
       .spi_csb_en_o({spi_cs_1_oe_o, spi_cs_0_oe_o}),
@@ -715,7 +716,7 @@ module core_v_mini_mcu #(
       .spi_intr_event_o(spi_intr),
       .spi_rx_valid_o(spi_rx_valid),
       .spi_tx_ready_o(spi_tx_ready),
-      .spi2_sck_o,
+      .spi2_sck_o(spi2_sck_o),
       .spi2_sck_en_o(spi2_sck_oe_o),
       .spi2_csb_o({spi2_cs_1_o, spi2_cs_0_o}),
       .spi2_csb_en_o({spi2_cs_1_oe_o, spi2_cs_0_oe_o}),
